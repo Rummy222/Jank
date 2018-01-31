190fa31be57d8e1b328f0becd6d343226f81ddac
David Rohm <dr9557@gmail.com>
2018-01-30 22:20:23 -0500

    20180130 - replacing the render-blocking css with inline css

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -3,3 +3,6 @@ var psi = require('psi');
 var sequence = require('run-sequence');
 var site = '';
 var portval = 3020;
+
+//this is where your server task goes - I'm using browser-sync
+gulp.task('browser-sync',)
